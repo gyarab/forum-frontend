35a04948ce36a9574fa0ff4b58f8bb721ccf9777
DavidCech <david.cech@student.gyarab.cz>
2018-11-18 10:31:05 +0100

    LilPost still doesn't work v2

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ class App extends Component {
         return (
             <Provider store={configureStore()}>
                 <div className="App">
-
-                        <Navigation/>
                     <header className="App-header">
                     </header>
                     <LilPost />
--- src/components/LilPost.js
+++ src/components/LilPost.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { Card, CardHeader, CardBody,
     CardText } from 'reactstrap';
 import '../styles/lilPost.css';
+import '../styles/colors.css';
 
 
 class Navigation extends Component {
@@ -14,9 +15,9 @@ class Navigation extends Component {
     render() {
         return (
             <div>
-                <Card >
-                    <CardHeader className={"card-header"}>Title of the post</CardHeader>
-                    <CardBody className={"card-body"}>
+                <Card color="blue">
+                    <CardHeader color="blue">Title of the post</CardHeader>
+                    <CardBody color="blue">
                         <CardText> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim. Etiam ligula pede, sagittis quis, interdum ultricies, scelerisque eu. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce nibh. In rutrum. Aliquam erat volutpat. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim. Morbi leo mi, nonummy eget tristique non, rhoncus non leo. In dapibus augue non sapien. Integer lacinia. Aliquam erat volutpat. Etiam ligula pede, sagittis quis, interdum ultricies, scelerisque eu. Morbi leo mi, nonummy eget tristique non, rhoncus non leo. Vivamus ac leo pretium faucibus. Integer vulputate sem a nibh rutrum consequat. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Aenean vel massa quis mauris vehicula lacinia.</CardText>
                     </CardBody>
                 </Card>
